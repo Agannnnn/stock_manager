84d25c6cf4c400d1a0a474434586c97a8909c35a
Sultan Ahmad <ahmad.sultan.2777@gmail.com>
2023-05-17 20:14:37 +0700

    Added form standard

---
+++ src/components/Form.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const emits = defineEmits(["closeForm"]);
+
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    emits("closeForm");
+  }
+});
+</script>
+
+<template>
+  <div class="w-screen h-screen absolute top-0 left-0 z-10">
+    <div
+      class="w-screen h-screen absolute top-0 left-0"
+      style="background-color: rgba(0, 0, 0, 0.5)"
+      @click="$emit('closeForm')"
+    ></div>
+    <slot></slot>
+  </div>
+</template>
