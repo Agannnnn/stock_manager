c5e9dfe6088ab9c49e545bfdfdd84a0c78b40763
Sultan Ahmad <ahmad.sultan.2777@gmail.com>
2023-05-17 19:10:13 +0700

    Some tweaking

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,25 +9,21 @@ const activeLinkClass = "text-gray-300 pointer-events-none";
     <Container class="flex justify-between items-center mt-3 mb-4">
       <div class="flex items-end gap-10">
         <h1 class="text-2xl text-primary font-bold">LOGO PERUSAHAAN</h1>
-        <ul class="text-lg text-primary font-medium flex flex-row gap-4">
-          <li>
-            <RouterLink
-              to="/"
-              :class="$route.name === 'items' ? activeLinkClass : ''"
-            >
-              DAFTAR ITEM
-            </RouterLink>
-          </li>
-          <li>|</li>
-          <li>
-            <RouterLink
-              to="/transactions"
-              :class="$route.name === 'transactions' ? activeLinkClass : ''"
-            >
-              RIWAYAT TRANSAKSI
-            </RouterLink>
-          </li>
-        </ul>
+        <div class="text-lg text-primary font-medium flex flex-row gap-4">
+          <RouterLink
+            to="/"
+            :class="$route.name === 'items' ? activeLinkClass : ''"
+          >
+            DAFTAR ITEM
+          </RouterLink>
+          <span>|</span>
+          <RouterLink
+            to="/transactions"
+            :class="$route.name === 'transactions' ? activeLinkClass : ''"
+          >
+            RIWAYAT TRANSAKSI
+          </RouterLink>
+        </div>
       </div>
     </Container>
   </nav>
