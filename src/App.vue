c0cfc4c9de47ef784a5fb2d870484df5535a6d78
Sultan Ahmad <ahmad.sultan.2777@gmail.com>
2023-05-17 19:00:14 +0700

    Tweaked some UI feature

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import Navbar from "./components/Navbar.vue";
 import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen">
     <Navbar />
-    <main class="flex-grow bg-secondary">
+    <main class="flex-grow bg-secondary px-2">
       <RouterView />
     </main>
   </div>
